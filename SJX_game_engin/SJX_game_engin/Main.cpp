578c499ae0ad3f14004adb124c94a7f0a86a582d
SJX1994 <752523247@qq.com>
2021-10-31 21:05:31 +0800

    add sort not finish

--- SJX_game_engin/SJX_game_engin/Main.cpp
+++ SJX_game_engin/SJX_game_engin/Main.cpp
@@ -2,24 +2,24 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
-const char* vertexShaderSource =
-"#version 330 core \n "
-"layout (location = 0) in vec3 aPos; \n"
-"void main() \n"
-"{ \n"
-"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); \n"
-"} \0"
-;
-
-const char* fragmentShaderSource =
-"#version 330 core \n "
-"layout (location = 0) \n"
-"out vec4 FragColor; \n"
-"void main() \n"
-"{ \n"
-"FragColor = vec4(0.3, 0.3, 0.0, 1.0); \n"
-"}\n \0"
-;
+//const char* vertexShaderSource =
+//"#version 330 core \n "
+//"layout (location = 0) in vec3 aPos; \n"
+//"void main() \n"
+//"{ \n"
+//"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); \n"
+//"} \0"
+//;
+//
+//const char* fragmentShaderSource =
+//"#version 330 core \n "
+//"layout (location = 0) \n"
+//"out vec4 FragColor; \n"
+//"void main() \n"
+//"{ \n"
+//"FragColor = vec4(0.3, 0.3, 0.0, 1.0); \n"
+//"}\n \0"
+//;
 
 
 int main() 
@@ -32,16 +32,26 @@ int main()
 	// tirangle
 	float  sq3;
 	sq3 = float(sqrt(3));
-
+	//顶点位置
 	GLfloat vertices[] =
 	{
 			-0.5f,-0.5f *sq3/3 , 0.0f,
 
 			0.5f,-0.5f *sq3/3 , 0.0f,
 
-			0.0f,0.5f *sq3*2 / 3 , 0.0f
-	};
+			0.0f,0.5f *sq3*2 / 3 , 0.0f,
 
+			-0.5/2, 0.5*sq3/6 , 0.0f,
+			0.5 / 2, 0.5*sq3 / 6 , 0.0f,
+			0.0 , -0.5*sq3 / 3 , 0.0f
+	};
+	//顶点绘制顺序
+	GLuint indices[] = 
+	{
+			0, 3, 5,
+			3, 2, 4,
+			5, 4, 1
+	};
 
 	GLFWwindow* window = glfwCreateWindow(800,800,"GameEnginSJX",NULL,NULL);
 
@@ -80,15 +90,21 @@ int main()
 
 
 	//Vertex and Buffer s' arrary
-	GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1,&VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
+
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+
 
 	glVertexAttribPointer(
 	0,
@@ -103,12 +119,13 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER,0);
 	glBindVertexArray(0);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-	glClearColor(0.0,0.1,0.0,1.0);
+	/*glClearColor(0.0,0.1,0.0,1.0);
 
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glfwSwapBuffers(window);
+	glfwSwapBuffers(window);*/
 
 	//show
 	while (!glfwWindowShouldClose(window))
@@ -117,7 +134,8 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		glfwPollEvents();
@@ -125,6 +143,7 @@ int main()
 
 	glDeleteVertexArrays(1,&VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 	glfwDestroyWindow(window);
---
+++ SJX_game_engin/SJX_game_engin/default.frag
@@ -0,0 +1,7 @@
+#version 330 core
+"layout (location = 0) 
+out vec4 FragColor; 
+void main() 
+{ 
+FragColor = vec4(0.3, 0.3, 0.0, 1.0);
+}
